be80443b03f0b8fd68b6c0d664ec84fbb973d1c7
Antonio Siqueira <antoniofsiqueirajr@gmail.com>
2023-11-30 14:20:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,40 @@
             <div class="text-right">Antonio Siqueira</div>
         </header>
         <div class="main flex flex-1"> 
-          
+            <div>
+                <div class="painel text-center flex h-center v-center">
+                    <i class="fa fa-2x fa-phone text-danger"></i>
+                    <span class="ml-5">&nbsp;&nbsp;TELEFONE</span>
+                </div>
+                <div class="flex">
+                    <div class="flex">
+                        <div class="painel bg-success flex-1">
+                            <div class="painel-titulo">Atrasado</div>
+                            <div class="painel-subtitulo">&nbsp;</div>
+                            <div class="painel-indicador">12</div>
+                        </div>
+                    </div>
+                    <div class="flex-1 flex flex-column">
+                        <div class="flex flex-1">
+                            <div class="painel flex-1">
+                                <div class="painel-titulo">Hoje</div>
+                                <div class="painel-subtitulo">&nbsp;</div>
+                                <div class="painel-indicador">0</div>
+                            </div>
+                            <div class="painel flex-1">
+                                <div class="painel-titulo">Em andamento</div>
+                                <div class="painel-subtitulo">&nbsp;</div>
+                                <div class="painel-indicador">1</div>
+                            </div>
+                        </div>
+                        <div class="painel flex-1">
+                            <div class="painel-titulo">Último</div>
+                            <div class="painel-subtitulo">&nbsp;</div>
+                            <div class="painel-indicador painel-indicador-pequeno">10:48:37</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div>
                 <div class="painel text-center flex h-center v-center">
                     <i class="fas fa-2x fa-comment-alt"></i>
